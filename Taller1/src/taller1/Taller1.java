4bdbe5d84df57ba9934061c609581c37e0b9d2fd
Estudiantes <Estudiantes@Labing70212.udistrital.edu.co>
2017-10-02 18:05:29 -0500

    Arreglado

--- Taller1/src/taller1/Taller1.java
+++ Taller1/src/taller1/Taller1.java
@@ -33,24 +33,45 @@ public class Taller1 {
         try {
             conexion = DriverManager.getConnection(url, usuario, clave);
             System.out.println("POR FIN!");
-            String cadena="INSERT INTO ejemplo(idejemplo, dato) VALUES (2,2)";
+            String cadena = "INSERT INTO ejemplo(idejemplo, datos) VALUES (1,1)";
             //1. Crear un objeto
             Statement sentencia;
-            sentencia  = conexion.createStatement();
+            sentencia = conexion.createStatement();
             sentencia.execute(cadena);
-            
-        } catch (SQLException ex) {
-            
-            
-            String cad=ex.getMessage();
-            System.out.println(ex);
-            
-        
-        //
-        
-        
-        
-    }
 
+        } catch (SQLException ex) {
+
+            String cad = ex.getMessage();
+            System.out.println("Error: "+cad);
+            String err1 = ".*database.*";
+            String err2 = "Table.*";
+            String err3 = "Duplicate entry.*";
+            String err4 = "You have an error in your SQL syntax.*";
+            String err5 = ".*column.*";
+            String err6 = ".*user.*";
+            String err7 = ".*suitable driver.*";
+            if (cad.matches(err1)) {
+                System.out.println("Base de datos desconocida");
+            }
+            if (cad.matches(err2)){
+                System.out.println("Error en la tabla");
+            }
+            if (cad.matches(err3)){
+                System.out.println("Entrada duplicada para la llave primaria");
+            }
+            if (cad.matches(err4)){
+                System.out.println("Error en sintaxis SQL");                
+            }
+            if (cad.matches(err5)){
+                System.out.println("Columna desconocida, ingrese nuevamente el nombre");                
+            }
+            if (cad.matches(err6)){
+                System.out.println("Error en el usuario o contraseña, verifique la conexión");                
+            }
+            if (cad.matches(err7)){
+                System.out.println("Verifique la URL de la conexión");                
+            }
+        }
+    
 }
 }
